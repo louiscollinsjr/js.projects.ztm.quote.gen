de5c7375320bcedf4788d2cec6e8c6004daa1226
louiscollinsjr <louiscollinsjr@gmail.com>
2024-05-10 23:40:39 +0300

    added loader

--- script.js
+++ script.js
@@ -5,11 +5,27 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
 
 let apiQuotes = [];
-// get quotes from api
 
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+ // hide loading
+ function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+ }
+
+// get quotes from api
 function newQuote() {
+    loading();
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
 
   // check author exsists
@@ -25,11 +41,13 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
-
+// set quote, hide loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 async function getQuotes() {
+    loading();
   const apiURL = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
   try {
     const reponse = await fetch(apiURL);
@@ -55,3 +73,4 @@ twitterBtn.addEventListener('click', tweetQuote);
 
 //On load
 getQuotes();
+
